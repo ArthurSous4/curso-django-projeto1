23203f725183e01bbf2a940a727a31382222ab0d
ArthurSous4 <arthur28lucas@gmail.com>
2023-12-29 18:34:23 -0300

    template for Receitas

--- recipes/views.py
+++ recipes/views.py
@@ -2,10 +2,5 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def home(request):
-    return HttpResponse('home')
+    return render(request, 'recipes/pages/home.html')
 
-def contato(request):
-    return HttpResponse('contato')
-
-def sobre(request):
-    return HttpResponse('sobre')
