891159c8e66dd7d88dc561eda9965b3bf6929d39
ArthurSous4 <arthur28lucas@gmail.com>
2024-01-11 13:41:03 -0300

    Criando nova view, separando arquivos static em uma pasta separada.

--- recipes/views.py
+++ recipes/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
-from django.http import HttpResponse
 
 def home(request):
-    return render(request, 'recipes/pages/home.html')
+    return render(request, 'recipes/pages/home.html', context={'name': 'Arthur Lucas',})
+
+def recipe(request, id):
+    return render(request, 'recipes/pages/home.html', context={'name': 'Arthur Lucas',})
 
