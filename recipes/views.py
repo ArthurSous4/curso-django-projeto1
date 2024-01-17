c4382901a700bc7c17921343a445d770bff57dce
ArthurSous4 <arthur28lucas@gmail.com>
2024-01-17 17:57:41 -0300

    criação do arquivo de factory para injeçao de dados

--- recipes/views.py
+++ recipes/views.py
@@ -4,5 +4,5 @@ def home(request):
     return render(request, 'recipes/pages/home.html', context={'name': 'Arthur Lucas',})
 
 def recipe(request, id):
-    return render(request, 'recipes/pages/home.html', context={'name': 'Arthur Lucas',})
+    return render(request, 'recipes/pages/recipe-view.html', context={'name': 'Arthur Lucas',})
 
