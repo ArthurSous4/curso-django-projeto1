904737a25968d8569e78b12efd0c6baa2147539f
ArthurSous4 <arthur28lucas@gmail.com>
2024-01-20 18:28:44 -0300

    Models

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,3 +1,13 @@
 from django.contrib import admin
+from .models import Category, Recipe
 
-# Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    ...
+
+@admin.register(Recipe)
+class RecipeAdimin(admin.ModelAdmin):
+    ...
+
+admin.site.register(Category, CategoryAdmin)
+
+    
